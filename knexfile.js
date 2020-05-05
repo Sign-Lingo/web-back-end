b819d440b5cf6926a5f658553a635f6c3f564512
Seth Cox <Sethcox16@yahoo.com>
2020-05-05 15:39:48 -0400

    added staging env

--- knexfile.js
+++ knexfile.js
@@ -48,6 +48,17 @@ module.exports = {
     },
   },
 
+  staging: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,

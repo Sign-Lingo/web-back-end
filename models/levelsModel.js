f1530c22e1d83864302f8206d555195825d50c49
cjogles <jackson.ogles28@gmail.com>
2020-06-05 22:58:44 -0600

    added checkUserLevels router and model

--- models/levelsModel.js
+++ models/levelsModel.js
@@ -4,4 +4,9 @@ module.exports = {
   getAllLevels() {
     return db("levels");
   },
+  checkUserLevels(userId) {
+    return db("user_levels")
+      .where("user_id", userId)
+      .select("*");
+  },
 };
--- routes/levelsRouter.js
+++ routes/levelsRouter.js
@@ -4,12 +4,22 @@ const Levels = require("../models/levelsModel");
 // get all levels
 router.get("/", (req, res) => {
   Levels.getAllLevels()
-    .then(levels => {
+    .then((levels) => {
       res.status(200).json(levels);
     })
-    .catch(error => {
-      res.status(500).json("Error getting all levels!", error )
+    .catch((error) => {
+      res.status(500).json("Error getting all levels!", error);
+    });
+});
+
+router.get("/check", (req, res) => {
+  Levels.checkUserLevels()
+    .then((userLevels) => {
+      res.status(200).json(userLevels);
     })
-})
+    .catch((error) => {
+      res.status(500).json("Error getting user levels!", error);
+    });
+});
 
 module.exports = router;
